ffa522a00ef7dc5eb52ac1325e34b21f9386654c
Teteuya <tetsuya.ueno@outlook.com>
2026-08-30 12:15:38 +0900

    refactor: rename ext4 host to hx90

--- flake.nix
+++ flake.nix
@@ -59,11 +59,11 @@
       };
 
       # x86_64 ext4 workstation using the same nixarchy desktop baseline.
-      nixos-ext4 = nixpkgs.lib.nixosSystem {
+      nixos-hx90 = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = { inherit inputs; };
         modules = [
-          ./hosts/nixos-ext4/configuration.nix
+          ./hosts/nixos-hx90/configuration.nix
         ];
       };
     };
