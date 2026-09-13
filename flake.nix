9ad82541de30aa40943722e53424a5493c838791
cheyan <iamcheyan@gmail.com>
2026-09-13 12:35:39 +0900

    chore: update NixOS and Nixarchy inputs

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
 
     nixarchy = {
-      url = "github:olafkfreund/nixarchy/v4.0.2-4";
+      url = "github:olafkfreund/nixarchy/v4.0.2-12";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
